48cee6f2dab8fe0e953f6d1d89ffbd32cda4b9eb
Amir Hossein Mohammadi <amir00462@gmail.com>
2025-07-19 19:44:39 +0300

    api_service code complete

---
+++ lib/constants.dart
@@ -0,0 +1,1 @@
+const String baseUrl = 'https://687a0f51abb83744b7eb3a98.mockapi.io/dunijet/api/v1';
--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,9 +1,43 @@
 import 'package:dio/dio.dart';
+import 'package:mockapi_flutter/constants.dart';
+import 'package:mockapi_flutter/models/user.dart';
 
 class ApiService {
-  final Dio dio = Dio();
+  final Dio dio = Dio(BaseOptions(baseUrl: baseUrl));
 
-  getUsers() {
-    Response response = dio.get('');
+  Future<List<User>> getUsers() async {
+    try {
+      Response response = await dio.get('/users');
+      List<dynamic> data = response.data;
+      return data.map((json) => User.fromJson(json)).toList();
+    } catch (e) {
+      throw Exception('Failed to load users: $e');
+    }
+  }
+
+  Future<User> createUser(String name, String city) async {
+    try {
+      Response response = await dio.post('/users', data: {'name': name, 'city': city});
+      return User.fromJson(response.data);
+    } catch (e) {
+      throw Exception('Failed to create user: $e');
+    }
+  }
+
+  Future<User> updateUser(String id, String name, String city) async {
+    try {
+      Response response = await dio.put('/users/$id', data: {'name': name, 'city': city});
+      return User.fromJson(response.data);
+    } catch (e) {
+      throw Exception('Failed to update user: $e');
+    }
+  }
+
+  Future<void> deleteUser(String id) async {
+    try {
+      await dio.delete('/users/$id');
+    } catch (e) {
+      throw Exception('Failed to delete user: $e');
+    }
   }
 }
