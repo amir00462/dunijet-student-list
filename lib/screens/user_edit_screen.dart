114c9bfcc6b5136f2b4d46727bd24bbfbb758b70
Amir Hossein Mohammadi <amir00462@gmail.com>
2025-08-15 12:26:40 +0300

    add TextFormField Name in user_edit_screen.dart

--- lib/screens/user_edit_screen.dart
+++ lib/screens/user_edit_screen.dart
@@ -11,8 +11,42 @@ class UserEditScreen extends StatefulWidget {
 }
 
 class _UserEditScreenState extends State<UserEditScreen> {
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController cityController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: Text(widget.user == null ? 'Add Student' : 'Edit Student')),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.all(24.0),
+          child: Form(
+            key: formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                TextFormField(
+                  controller: nameController,
+                  decoration: const InputDecoration(
+                    labelText: 'Student Name',
+                    prefixIcon: Icon(Icons.person_outline),
+                  ),
+                  validator: (value) {
+                    if (value == null || value.trim().isEmpty) {
+                      return 'Please Enter a Name';
+                    }
+
+                    return null;
+                  },
+                  textInputAction: TextInputAction.next,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
